99003578df455b8939c79cc7dc6701ea2028600b
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2026-02-20 18:11:30 +0700

    p011

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,20 @@
-export class User {}
+export class User {
+    firstname:string = '';
+    lastname:string = '';
+    private age:number = 0
+    static BIRTH_YEAR = 2000
+
+
+    setFirstname(){
+        this.firstname = firstname
+    }
+    getFullName() {
+
+    }
+
+    getAge(){
+
+    }
+
+    
+}
