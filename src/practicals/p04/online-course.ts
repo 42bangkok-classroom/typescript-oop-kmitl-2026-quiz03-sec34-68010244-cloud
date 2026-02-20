2898bf1adec558b7eaf808a952f382b94a22e9e5
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2026-02-20 18:57:44 +0700

    p04

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,39 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+  courseName: string
+  maxStudents: number
+  private enrolledStudents: number
+  private isOpen: boolean = true
+
+  constructor(courseName: string, maxStudents: number) {
+    this.courseName = courseName
+    this.maxStudents = maxStudents
+    this.enrolledStudents = 0
+  }
+
+  private canEnroll(): boolean {
+    return this.enrolledStudents < this.maxStudents
+  }
+
+  enroll(): boolean {
+    if (this.canEnroll() && this.isOpen) {
+      this.enrolledStudents++
+      return true
+    }
+    return false
+  }
+
+
+  getCourseStatus(): string {
+    return this.isOpen ? "Open" : "Closed";
+  }
+
+  
+   getAvailableSeats(): number {
+    return this.maxStudents - this.enrolledStudents;
+  }
+
+  closeCourse(): void {
+    this.isOpen = false;
+  }
+ 
+}
